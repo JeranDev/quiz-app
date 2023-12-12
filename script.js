99edbb583d09850259788ec2ec6288e97b2d89d7
jeran <jeranb@protonmail.com>
2023-12-11 21:35:08 -0700

    finished

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const form = document.querySelector('#quiz-form')
-const answers = document.querySelectorAll('.answer')
+const form = document.getElementById('quiz-form')
+const answers = Array.from(document.querySelectorAll('.answer'))
+const questionItems = document.querySelectorAll('.question-item')
+const alert = document.querySelector('#alert')
 
-document.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
   e.preventDefault()
 
-  for (let i = 0; i < answers.length; i++) {
-    if (answers[i].checked) {
-      const parent = answers[i].closest('.question-item')
-      if (answers[i].value == 'true') {
-        parent.classList.add('correct')
-        parent.classList.remove('incorrect')
-      } else {
-        parent.classList.add('incorrect')
-        parent.classList.remove('correct')
-      }
+  questionItems.forEach(questionItem => {
+    questionItem.classList.add('incorrect')
+    questionItem.classList.remove('correct')
+  })
+
+  const checkedAnswers = answers.filter(answer => answer.checked)
+  checkedAnswers.forEach(answer => {
+    const isCorrect = answer.value === 'true'
+    const questionItem = answer.closest('.question-item')
+
+    if (isCorrect) {
+      questionItem.classList.add('correct')
+      questionItem.classList.remove('incorrect')
+    } else {
+      questionItem.classList.add('incorrect')
+      questionItem.classList.remove('correct')
     }
-  }
+
+    const allTrue = checkedAnswers.every(answer => answer.value === 'true')
+    const allAnswered = checkedAnswers.length === questionItems.length
+
+    if (allTrue && allAnswered) {
+      alert.classList.add('active')
+      setTimeout(() => {
+        alert.classList.remove('active')
+      }, 1000)
+    }
+  })
 })
